1d1d2fc312586adc2ec84a9d248d997b7202a68b
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2022-12-13 20:49:20 -0500

    Update 1551 Minimum Operations to Make Array Equal.py

--- 1551 Minimum Operations to Make Array Equal.py
+++ 1551 Minimum Operations to Make Array Equal.py
@@ -4,3 +4,7 @@ class Solution(object):
         for i in range(1,n,2):
             count=count+(n-i)
         return count
+
+    class Solution(object):
+    def minOperations(self, n):
+        return (n//2)*((n+1)//2)

eeb2e74e7f2218560ed02182020e3c495fd582a6
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-01-23 18:11:20 -0500

    Update 1704. Determine if String Halves Are Alike.py

--- 1704. Determine if String Halves Are Alike.py
+++ 1704. Determine if String Halves Are Alike.py
@@ -10,3 +10,15 @@ class Solution:
             if l[i] in vow:
                 count-=1
         return True if count==0 else False
+
+    
+    #Second solution
+def count_vowels(s:str)->int:
+    return sum(c.lower() in 'aeiou' for c in s)
+
+
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        m = len(s) // 2
+        a, b=s[:m], s[m:]
+        return count_vowels(a)==count_vowels(b)

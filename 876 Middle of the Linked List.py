aa46f37d1a8c5878340270b276fba7059ccc6466
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-01-26 19:46:58 -0500

    Update 876 Middle of the Linked List.py

--- 876 Middle of the Linked List.py
+++ 876 Middle of the Linked List.py
@@ -9,3 +9,14 @@ class Solution:
         while fast and fast.next:
             slow, fast = slow.next, fast.next.next
         return slow
+
+
+#Second
+class Solution:
+     def middleNode(self, head: ListNode) -> ListNode:
+        arr=[]
+        node=head
+        while node is not None:
+          arr.append(node)
+          node=node.next
+        return arr[len(arr)//2]

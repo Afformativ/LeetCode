fb5c6cb8b5634ab10571b0ee7c7eef80074d2a98
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-06-22 22:47:16 -0400

    Update 9 Palindrome Number.py

--- 9 Palindrome Number.py
+++ 9 Palindrome Number.py
@@ -7,3 +7,26 @@ class Solution(object):
             res = res * 10 + r % 10
             r /= 10
         return res == x
+
+#Second solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x)[0:] == str(x)[::-1]:
+            return True
+        return False 
+
+#Third solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x <0:
+            return False
+
+        new=0
+        orig=x
+
+        while x:
+            x,d=divmod(x,10)
+            new=new*10+d
+
+        return new==orig
+            

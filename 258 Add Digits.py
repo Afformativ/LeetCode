898bf221e2d04ea8fbaab9d1d0cbcd852f37c570
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-06-23 23:16:31 -0400

    Update 258 Add Digits.py

--- 258 Add Digits.py
+++ 258 Add Digits.py
@@ -1,3 +1,14 @@
 class Solution(object):
     def addDigits(self, num):
         return num%9 or num and 9
+#Second Solution
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >=10:
+            cur=num
+            new_num=0
+            while cur:
+                cur,d=divmod(cur,10)
+                new_num+=d
+            num=new_num
+        return num

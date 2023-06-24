60e3edc655ba6fc1ef225517ec14fbbdd46eb511
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-06-23 22:28:59 -0400

    Create 929. Unique Email Addresses.py

---
+++ 929. Unique Email Addresses.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique = set()
+
+        for e in emails:
+
+            name,dom=e.split('@')
+
+            name=name.split('+')[0]
+            name=name.replace('.','')
+
+            unique.add(f'{name}@{dom}')
+
+        return(len(unique))

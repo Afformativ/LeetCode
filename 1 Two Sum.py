dacfe36c9d9243622bd4935997c19f5826caa086
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-06-26 16:50:51 -0400

    Update 1 Two Sum.py

--- 1 Two Sum.py
+++ 1 Two Sum.py
@@ -4,3 +4,13 @@ class Solution(object):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
                     return[i,j]
+
+#Second Solution
+class Solution(object):
+    def twoSum(self, nums, target):
+        d={}
+        for i in range(len(nums)):
+            if nums[i] not in d:
+                d[(target-nums[i])]=i
+            else:
+                return[i,d[nums[i]]]

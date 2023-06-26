b81e0178c23705b405ee6038d50eea922028cb22
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-06-25 22:14:41 -0400

    Update 118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -9,3 +9,21 @@ class Solution:
         ans = []
         helper(n)
         return ans
+
+#Second solution
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:        
+        dp=[[1],[1,1]]
+
+        if n <3:
+            return dp[:n]
+
+        
+        for _ in range(n-2):
+            next_row=[1]
+            for i in range(1,len(dp[-1])):
+                next_row.append(dp[-1][i]+dp[-1][i-1])
+
+            next_row+=[1]
+            dp.append(next_row)
+        return dp

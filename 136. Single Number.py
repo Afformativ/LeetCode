7579eed867856208e832a1389d90a3c5ac6e5753
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-06-25 22:17:30 -0400

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,3 +1,12 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return(sum(set(nums))*2-sum(nums))
+
+#Second solution
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans=0
+
+        for num in nums:
+            ans^=num
+        return ans

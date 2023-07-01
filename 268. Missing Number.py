ca6723e08691c3dbe2fe4f97bd126e3e151068b1
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-07-01 13:38:02 -0400

    Update 268. Missing Number.py

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -5,3 +5,7 @@ class Solution:
             if i not in nums:
                 return i
         return max(nums)+1
+#Second Solution
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return list(set(range(len(nums)+1))-set(nums))[0]

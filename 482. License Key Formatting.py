b0e8743e95f3c01d4f96905ef1b340e202e380c1
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-07-06 23:21:09 -0400

    Create 482. License Key Formatting.py

---
+++ 482. License Key Formatting.py
@@ -0,0 +1,9 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int) -> str:
+        s=s.replace('-','').upper()
+        ans=''
+        for i in range(len(s)-1,-1,-1):
+            if not len(ans)%(k+1):
+                ans+='-'
+            ans+=s[i]
+        return ans[::-1][:-1]

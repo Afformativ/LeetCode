2748313b0a5e65f53a84bd3187800cf5e4a839d6
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-07-06 23:32:53 -0400

    Update 169. Majority Element.py

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -7,3 +7,20 @@ class Solution:
         key_list = list(dic.keys())
         val_list = list(dic.values())
         return key_list[val_list.index(max(val_list))]
+
+#Second Solution
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        return Counter(nums).most_common(1)[0][0]
+
+#Third Solution
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count=0
+        candidate=None
+
+        for num in nums:
+            if count==0:
+                candidate=num
+            count+=(1 if num==candidate else -1)
+        return candidate

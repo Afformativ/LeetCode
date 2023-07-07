dd344c20c270744ba583a90066ceadd97fa7612f
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-07-06 23:42:59 -0400

    Update 94. Binary Tree Inorder Traversal.py

--- 94. Binary Tree Inorder Traversal.py
+++ 94. Binary Tree Inorder Traversal.py
@@ -10,3 +10,24 @@ class Solution:
             return []
         else:
             return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right)
+
+#Second Solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans=[]
+        stack=[]
+        cur=root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur=cur.left
+            cur=stack.pop()
+            ans.append(cur.val)
+            cur=cur.right
+        return ans

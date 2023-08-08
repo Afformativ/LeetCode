c323377ff06cd326604a8d1aa75d6c52984d1aaa
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2023-08-08 18:24:46 -0400

    Create 2149. Rearrange Array Elements by Sign.cpp

---
+++ 2149. Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> pos,neg,res;
+        for (int i =0; i<nums.size();++i){
+            if (nums[i]>0){
+                pos.push_back(nums[i]);
+            }
+            else{
+                neg.push_back(nums[i]);
+            }
+        }
+        for(int j=0;j<pos.size();++j){
+            res.push_back(pos[j]);
+            res.push_back(neg[j]);
+        }
+        return res;
+    }
+};

a08a8fd209daa6a7220a44b51f35b239f5a741b1
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2024-09-09 11:11:10 -0400

    Update 2824. Count Pairs Whose Sum is Less than Target.py

--- 2824. Count Pairs Whose Sum is Less than Target.py
+++ 2824. Count Pairs Whose Sum is Less than Target.py
@@ -6,4 +6,24 @@ class Solution:
                 if (nums[i]+nums[j])<target:
                     cnt+=1
         return cnt
+//Second Solution with Binary search
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums = sorted(nums)
+        res=0
+        for i,n in enumerate(nums):
+            value=target-n
+            res+=self.binSearch(i+1,value,nums)
+        return res
+
+    def binSearch(self,start,value,nums):
+        l,h=start,len(nums)
+        while l<h:
+            mid=(l+h)//2
+            if nums[mid]< value:
+                l=mid+1
+            else:
+                h=mid
+        return l-start
+
         

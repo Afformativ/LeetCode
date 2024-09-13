8549f69c88afc221ad19591b633edb4c692990ea
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2024-09-13 17:01:31 -0400

    Update 283 Move Zeroes.py

--- 283 Move Zeroes.py
+++ 283 Move Zeroes.py
@@ -18,3 +18,25 @@ class Solution:
             if nums[i]!=0:
                 nums[i],nums[j]=nums[j],nums[i]
                 j+=1
+#Third Solution
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l,r=0,1
+        while l<len(nums) and r<len(nums):
+            if nums[l]!=0:
+                l+=1
+                r+=1
+            elif nums[l]==0:
+                while r<len(nums) and nums[r]==0 :
+                    r+=1
+                if r<len(nums):
+                    temp=nums[l]
+                    nums[l]=nums[r]
+                    nums[r]=temp
+                    l+=1
+                    r+=1
+
+

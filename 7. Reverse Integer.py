5fee44d5f3147e2236811395c56d6c0c8a3300b5
Sasha Hrabar <40630611+Afformativ@users.noreply.github.com>
2024-09-20 13:25:44 -0400

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -17,3 +17,25 @@ class Solution:
                 return 0
             else:
                 return x
+#Second Solution
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x==0:
+            return 0
+        nums=list(str(x))
+        l,r=0,len(nums)-1
+        res=[]
+        while nums[r]=='0':
+            r-=1
+        while l<=r:
+            if nums[l]=='-':
+                res.append(nums[l])
+                l+=1
+            else:
+                res.append(nums[r])
+                r-=1
+        k=int(''.join(res)) 
+        if k>2 ** 31 - 1 or k<-2 ** 31:
+            return 0
+        else:
+            return k
